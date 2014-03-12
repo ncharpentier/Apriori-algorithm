a351f925b0892dec141daf94b24fd72f33028711
BARON <raphael.baron@insa-rennes.fr>
2014-03-12 09:01:31 +0100

    Ajout d'Item

---
+++ src/com/apriori/data/Item.java
@@ -0,0 +1,25 @@
+package apriori;
+
+import apriori.Item;
+
+public class Item {
+	private int id;
+	private String description;
+	
+	public Item(int id, String des){
+		this.id = id;
+		this.description = des;
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	public boolean isEqual(Item c) {
+		return this.id == c.id;
+	}
+	}

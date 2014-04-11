e5c4f7d00ebb326f503a0853183abf1117586904
unknown <raphael.baron@insa-rennes.fr>
2014-04-11 19:44:23 +0200

    Correction du Script : Presence des mots en debut et fin de lignes prise en compte

--- Resources/unstructured/Script.pl
+++ Resources/unstructured/Script.pl
@@ -1,6 +1,6 @@
 	#!/usr/bin/perl
 
-	open(ARTICLE,"<articles_petit_10_pourcent.txt") || die ("Erreur d'ouverture de ARTICLE");
+	open(ARTICLE,"<articles_grand_100_pourcent.txt") || die ("Erreur d'ouverture de ARTICLE");
 	open(SORTIE_APRIORI,">transa.txt") || die ("Erreur d'ouverture de SORTIE_APRIORI");
 	open(MOTS,"<mots.lst") || die ("Erreur d'ouverture de MOTS");
 	open(CONFIG,">config.txt") || die ("Erreur d'ouverture de CONFIG");
@@ -34,13 +34,11 @@
 		$premier_mot = 1; # obligatoire sinon virgule en debut de ligne
 		foreach (@out) {
 		      $mot = $_;
-		      $mot = " " . $mot . " ";  # on ajoute des espaces autour du mot afin de ne matcher que lui
-
-			  if(not $premier_mot) {        # gestion de la virgule
+		      if(not $premier_mot) {        # gestion de la virgule
 		               print SORTIE_APRIORI "\t";
 		      }
 		      $premier_mot = 0;
-		      if($ligne =~ m/$mot/) {
+		      if($ligne =~ /\b$mot\b/) {
 		     	 print SORTIE_APRIORI "1";      # ecriture du mot sans les espaces
 		      } else { print SORTIE_APRIORI "0"}
 		      
